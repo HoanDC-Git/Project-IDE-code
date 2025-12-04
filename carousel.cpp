03a023fd9d6f35064fd8f7c7e0a66e1971e41753
HoanDC-Git <hoandc203@gmail.com>
2025-12-04 10:27:21 +0700

    Carousel_config.cpp
    Change logic, using two sensors instead of calculate step

--- carousel.cpp
+++ carousel.cpp
@@ -9,7 +9,8 @@
 #define Carousel_Motor_Step_Pin         54
 #define Carousel_Motor_Dir_Pin          55
 #define Carousel_Motor_En_Pin           38
-#define Carousel_Motor_LimitSwitch_Pin  3
+#define Carousel_Motor_Home_Pin         2 // X+ Brown = VCC, Black = Signal, Blue = GND
+#define Carousel_Motor_Countshelf_Pin   3 // X-   0 co vat, 1 khong co vat
 
 // // //---------------------------------------------------------------// // //
 // // AccelStepper(interface, stepPin, dirPin)
@@ -19,28 +20,20 @@ AccelStepper carouselMotor(AccelStepper::DRIVER, Carousel_Motor_Step_Pin, Carous
 // // ---------------- CONSTANTS ----------------- // //
 // Carousel Motor
 const int CAROUSEL_MOTOR_MICROSTEP        = 4;
-const int CAROUSEL_MOTOR_MAXSPEED         = 200;
-const int CAROUSEL_MOTOR_HOMESPEED        = - CAROUSEL_MOTOR_MAXSPEED / 2;
-const long MAX_CAROUSEL_POS               = 123445L; //##############################//
-const float CAROUSEL_GEARBOX_RATIO        = 5.18;
-const float CAROUSEL_WHEEL_DIAMETER       = 487.33;
-const float CAROUSEL_WHEEL_CIRCUMFERENCE  = CAROUSEL_WHEEL_DIAMETER * 3.14159;
-const float SHELF_SPACING                 = 254.044;
+const int CAROUSEL_MOTOR_MAXSPEED         = -100;
+const int CAROUSEL_MOTOR_HOMESPEED        = CAROUSEL_MOTOR_MAXSPEED;
+const int SHELF_NUMBER                    = 8;
 
 const int ACCELERATION                    = 2000;
-const long INF                            = 31231025325L;
-const int STEP_PER_ROUND                  = 200;
 
 
 // // ---------------- VARIABLES ----------------- // //
-int last_switch_state                     = HIGH;
-unsigned long last_debounce_time          = 0;
-const unsigned long debounce_delay        = 50;
+int carousel_current_position             = 10;
 
 // // //---------------------------------------------------------------// // //
 // // ---------------- SETUP ----------------- // // //
 void setup() {
-  Serial.begin(115200);
+  Serial.begin(9600);
   Serial.println("San sang nhan lenh...");
 
   // --- Cấu hình động cơ Carousel ---
@@ -49,9 +42,53 @@ void setup() {
   carouselMotor.setMaxSpeed(CAROUSEL_MOTOR_MAXSPEED * CAROUSEL_MOTOR_MICROSTEP);
   carouselMotor.setAcceleration(ACCELERATION * CAROUSEL_MOTOR_MICROSTEP);
   carouselMotor.enableOutputs();
-  pinMode(Carousel_Motor_LimitSwitch_Pin, INPUT_PULLUP);
+  pinMode(Carousel_Motor_Home_Pin, INPUT);
+  pinMode(Carousel_Motor_Countshelf_Pin, INPUT);
+
 }
 
+void homing() {
+  carouselMotor.setSpeed(CAROUSEL_MOTOR_HOMESPEED * CAROUSEL_MOTOR_MICROSTEP);
+  while (digitalRead(Carousel_Motor_Home_Pin) == 1) {
+    carouselMotor.runSpeed();
+  }
+  carouselMotor.stop();
+  carousel_current_position = 2;
+}
+
+void move (int spacing) {
+  if (spacing == 0) return;
+  carouselMotor.setSpeed(CAROUSEL_MOTOR_MAXSPEED * CAROUSEL_MOTOR_MICROSTEP);
+  int count = 0;
+  int current_sensor_state = 0;
+  int last_sensor_state = 0;
+  unsigned long current_time = millis();
+  unsigned long last_debounce_time = millis();
+  int min_debounce = 500;
+
+  while (count < spacing) {
+    current_time = millis();
+    current_sensor_state = digitalRead(Carousel_Motor_Countshelf_Pin);
+    carouselMotor.runSpeed();
+
+    if (current_sensor_state != last_sensor_state) {
+      if (current_sensor_state == 0 && (current_time - last_debounce_time) > min_debounce) {
+        last_sensor_state = current_sensor_state;
+        last_debounce_time = current_time;
+        count++;
+
+        Serial.print("Da di qua:"); //Debug, nho cmt khi chay that ////////////////////////
+        Serial.print(count);
+        Serial.println(" ke");
+      }
+      else if ((current_time - last_debounce_time) > min_debounce) {
+        last_sensor_state = current_sensor_state;
+        last_debounce_time = current_time;
+      }
+    }
+  }
+  carouselMotor.stop();
+}
 // // //---------------------------------------------------------------// // //
 // // ---------------- PROCEDURE ----------------- // //
 
@@ -59,17 +96,22 @@ void setup() {
 // // ---------------- MAIN FUNCTION ----------------- // // //
 void loop() {
   if (Serial.available() > 0) {
-    int command = Serial.read();
-    if (command == 0) {
-      carouselMotor.moveTo(-INF);
-      Serial.println("Nhan cong tac de dung...");
-      while (digitalRead(Carousel_Motor_LimitSwitch_Pin) == HIGH) { carouselMotor.run(); }
-      carouselMotor.stop();
-      while (carouselMotor.distanceToGo() != 0) { carouselMotor.run(); }
-      carouselMotor.setCurrentPosition(0);
+    int command = Serial.parseInt();
+
+    // Xóa bộ đệm Serial khỏi các ký tự còn lại (ví dụ: '\r', '\n')
+    while (Serial.available()) {
+      Serial.read();
     }
-    else if (command > 0) { carouselMotor.move(command * SHELF_SPACING / CAROUSEL_WHEEL_CIRCUMFERENCE * CAROUSEL_GEARBOX_RATIO * CAROUSEL_MOTOR_MICROSTEP * STEP_PER_ROUND); }
-    else {                  carouselMotor.move(command * SHELF_SPACING / CAROUSEL_WHEEL_CIRCUMFERENCE * CAROUSEL_GEARBOX_RATIO * CAROUSEL_MOTOR_MICROSTEP * STEP_PER_ROUND); }
+
+    if (command == 1000) {
+      homing();
+    }
+    else if (command != 0) {
+      int spacing = (command - carousel_current_position + SHELF_NUMBER) % SHELF_NUMBER;
+      move(spacing);
+      carousel_current_position = command;
+      Serial.print("DONE    ");
+      Serial.println(carousel_current_position);
+    }
   }
-  carouselMotor.run();
 }
